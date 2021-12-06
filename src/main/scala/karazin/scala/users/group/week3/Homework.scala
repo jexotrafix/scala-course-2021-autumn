098f2a50aaee3925585868dad5ca96f4ac61343c
Игнатов Артур <ignatov.artur22@gmail.com>
2021-12-06 11:51:22 +0200

    Week-3

--- src/main/scala/karazin/scala/users/group/week3/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week3/Homework.scala
@@ -1,12 +1,13 @@
 package karazin.scala.users.group.week3
 
+import scala.annotation.tailrec
+
 object Homework:
   
   // Peano numbers
   abstract class Nat:
     def isZero: Boolean
     def predecessor: Nat
-    def successor: Nat = ???
     
     infix def + (that: Nat): Nat
     
@@ -16,9 +17,19 @@ object Homework:
     def toInt: Int
     
     // Optional task
-    def fromInt(int: Int) = ???
+    def fromInt(int: Int): Nat = {
+      if (int < 0) throw new Exception("Breaking the closeness of natural numbers")
+
+      @tailrec
+      def inner(int: Int, acc: Nat): Nat = int match {
+        case 0 => Zero
+        case _ => inner(int - 1, Succ(acc))
+      }
+
+      inner(int, Zero)
+    }
   
-    override def toString: String = s"Nat($predecessor)"
+    override def toString: String = s"Succ($predecessor)"
   
   type Zero = Zero.type 
   object Zero extends Nat:
@@ -27,24 +38,47 @@ object Homework:
     
     infix def +(that: Nat): Nat = that
     
-    infix def -(that: Nat): Nat = 
+    infix def -(that: Nat): Nat = that match {
+      case z: Zero => this
+      case _       => throw new Exception("Breaking the closeness of natural numbers")
+    } 
     
     // Optional task
     def toInt: Int = 0
 
     override def toString: String = "Zero"
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj match {
+      case z: Zero        => true
+      case _              => false
+    }
 
   class Succ(n: Nat) extends Nat:
     def isZero: Boolean = false
-    def predecessor: Nat = ???
+    def predecessor: Nat = n
     
-    infix def +(that: Nat): Nat = ???
+    infix def +(that: Nat): Nat = that match {
+      case z: Zero => this
+      case _       => Succ(this.predecessor) + that.predecessor
+    }
     
-    infix def -(that: Nat): Nat = ???
+    infix def -(that: Nat): Nat = that match {
+      case z: Zero => this
+      case _       => this.predecessor - that.predecessor
+    }
     
     // Optional task
-    def toInt: Int = ???
+    def toInt: Int = {
+      @tailrec
+      def inner(n: Nat, acc: Int): Int = n match {
+        case z: Zero => acc
+        case _       => inner(n.predecessor, acc + 1)
+      }
 
-    override def equals(obj: Any): Boolean = ???
+      inner(this, 0)
+    }
+
+    override def equals(obj: Any): Boolean = obj match {
+      case succ: Succ     => this.predecessor == succ.predecessor
+      case _              => false 
+    }
 
--- src/test/scala/karazin/scala/users/group/week3/HomeworkSpecification.scala
+++ src/test/scala/karazin/scala/users/group/week3/HomeworkSpecification.scala
@@ -5,29 +5,80 @@ import org.scalacheck._
 import Prop.{forAll, propBoolean, throws}
 import Homework._
 
-object HomeworkSpecification extends Properties("Homework"):
-
-  ???
-
-end HomeworkSpecification
-
+import arbitraries.{given Arbitrary[Nat], given Arbitrary[Zero], given Arbitrary[Succ], given Arbitrary[Int]}
 
 object ZeroSpecification extends Properties("Zero"):
+  property("is zero cheeeck") = forAll { (z: Zero) =>
+    z.isZero
+  }
 
-  ???
+  property("zero predecessor cheeeck") = forAll { (z: Zero) =>
+    throws(classOf[Exception]) {
+      z.predecessor
+    }
+  }
+
+  property("equals cheeeck") = forAll { (z: Zero, n: Nat) =>
+    (z equals n) == n.isInstanceOf[Zero]
+  }
+
+  property("plus operator cheeeck") = forAll { (z: Zero, n: Nat) =>
+    (z + n) equals n
+  }
+
+  property("minus operator cheeeck") = forAll { (z: Zero, n: Nat) =>
+    if (n.isInstanceOf[Zero]) (z - n) equals Zero
+    else throws(classOf[Exception]) {
+      z - n
+    }
+  }
+
+  property("to int cheeeck") = forAll { (z: Zero) =>
+    z.toInt == 0
+  }
 
 end ZeroSpecification
 
 
 object SuccSpecification extends Properties("Succ"):
+  property("is zero cheeeck") = forAll { (s: Succ) =>
+    s.isZero == false
+  }
 
-  ???
+  property("succ predecessor cheeeck") = forAll { (n: Nat) =>
+    Succ(n).predecessor == n
+  }
+
+  property("succ equals cheeeck") = forAll { (s1: Succ, s2: Succ) =>
+    s1.equals(s2) == s1.predecessor.equals(s2.predecessor)
+  }
+
+  property("to int cheeeck") = forAll { (i: Int, n: Succ) =>
+    n.fromInt(i).toInt == i
+  }
+
+  property("plus operator cheeeck") = forAll { (s1: Succ, s2: Succ) =>
+    (s1 + s2).toInt == (s1.toInt + s2.toInt) 
+  }
+
+  property("minus operator cheeeck") = forAll { (i1: Int, i2: Int, n: Nat) =>
+    if (i1 < i2) throws(classOf[Exception]) {
+      n.fromInt(i1) - n.fromInt(i2)
+    }
+    else (n.fromInt(i1) - n.fromInt(i2)).toInt == i1 - i2
+  } 
 
 end SuccSpecification
 
 object NatSpecification extends Properties("Nat"):
  
-  ???
+  property("from int cheeeck") = forAll { (i: Int, n: Succ) =>
+    (n.toInt == i) == n.fromInt(i).equals(n)
+  }
+
+  property("to string cheeeck") = forAll { (i: Int, n: Succ) =>
+    n.fromInt(i).toString == ("Succ(" * i + "Zero" + ")" * i)
+  }
 
 end NatSpecification
   
